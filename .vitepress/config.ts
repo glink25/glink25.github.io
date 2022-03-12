da3a0a42d3d4608daed488e533a57a244db02300
glink25 <guolinkai25@outlook.com>
2022-03-12 15:16:46 +0800

    feat: pwa assets

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -4,6 +4,30 @@ import getCustomConfig from "./utils/get-config";
 export default getCustomConfig({
   title: "Blog",
   description: "",
+  head: [
+    [
+      "meta",
+      {
+        name: "viewport",
+        content:
+          "width=device-width,initial-scale=1,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no",
+      },
+    ],
+    ["link", { rel: "icon", href: "/favicon.ico" }],
+    [
+      "link",
+      { rel: "apple-touchicon", href: "/apple-touch.ico", size: "180x180" },
+    ],
+    ["meta", { name: "theme-color", content: "#000" }],
+    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+    [
+      "meta",
+      {
+        name: "apple-mobile-web-app-status-bar-style",
+        content: "black-translucent",
+      },
+    ],
+  ],
   catagories: [
     { name: "tech", folder: "posts/tech", title: "Tech" },
     { name: "story", folder: "posts/story", title: "Story" },
