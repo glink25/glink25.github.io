5077fae343ff056cebd39443821416d022df8de4
Glink <49364151+glink25@users.noreply.github.com>
2024-11-09 14:55:34 +0800

    Update urodele.config.ts

--- urodele.config.ts
+++ urodele.config.ts
@@ -1,7 +1,7 @@
 export const config = {
   github: {
     login: 'glink25', // github login name, not user name
-    repo: "test-for-anything", //"urodele",
+    repo: "glink25.github.io", //"urodele",
   },
 } as const;
 
