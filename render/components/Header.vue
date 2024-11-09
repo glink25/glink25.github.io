e6cb8b76aa0be6c0c4e1e4c3d74dc1ed58c850f0
glink25 <guolinkai25@outlook.com>
2024-11-09 15:09:03 +0800

    feat: style

--- render/components/Header.vue
+++ render/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex justify-between items-center p-2 h-10">
-    <RouterLink to="/">
-      <div class="i-mdi-light:menu"></div>
+    <RouterLink to="/" class="text-button">
+      <div class="i-ri:menu-4-fill"></div>
     </RouterLink>
     <div class="flex gap-2 items-center header-operations">
       <div class="order-[4]">
