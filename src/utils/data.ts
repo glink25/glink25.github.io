3e60807cfe07b5e1cc0a7aa1f49d9c0609a09ed9
glink25 <guolinkai25@outlook.com>
2024-11-15 21:10:05 +0800

    refactor: draft

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,6 +1,6 @@
-import type { PageData } from "../shared/type";
+import type { ShortPageData } from "../shared/type";
 
-let globalData: PageData[] | undefined;
+let globalData: ShortPageData[] | undefined;
 export const getGlobalData = async () => {
   if (globalData) return globalData;
   globalData = await (await fetch(`${location.origin}/api/post/list`)).json();
