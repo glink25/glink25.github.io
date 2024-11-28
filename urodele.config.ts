cba894c22da540e0d9096ae6b3fe0efd8814c45e
glink02 <glink25@github.com>
2024-11-28 17:37:31 +0800

    feat: giscus

--- urodele.config.ts
+++ urodele.config.ts
@@ -1,9 +1,9 @@
 export const config = {
   github: {
-    login: 'glink25', // github login name, not user name
+    login: "glink25", // github login name, not user name
     repo: "glink25.github.io", //"urodele",
-    logInUrl: 'https://github-login.link-ai.workers.dev/',
-    logInAuthUrl: 'https://github-login.link-ai.workers.dev/',
+    logInUrl: "https://github-login.link-ai.workers.dev/",
+    logInAuthUrl: "https://github-login.link-ai.workers.dev/",
   },
   head: {
     title: "Urodele",
@@ -12,7 +12,22 @@ export const config = {
     copyright: "© Glink",
     copyrightUrl: "https://github.com/glink25",
   },
-  giscus: false as object | false,
+  giscus: {
+    src: "https://giscus.app/client.js",
+    "data-repo": "glink25/glink25.github.io",
+    "data-repo-id": "R_kgDONTJ_KQ",
+    "data-category": "Announcements",
+    "data-category-id": "DIC_kwDONTJ_Kc4Ckrg6",
+    "data-mapping": "pathname",
+    "data-strict": "0",
+    "data-reactions-enabled": "1",
+    "data-emit-metadata": "0",
+    "data-input-position": "bottom",
+    "data-theme": "preferred_color_scheme",
+    "data-lang": "zh-CN",
+    crossorigin: "anonymous",
+    async: true,
+  } as object | false,
 } as const;
 
 export default config;
