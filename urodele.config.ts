7b5222983b58d3701810e86193b2ca7396dce9b0
glink02 <glink25@github.com>
2024-12-02 17:57:27 +0800

    fix: login url

--- urodele.config.ts
+++ urodele.config.ts
@@ -2,8 +2,9 @@ export const config = {
   github: {
     login: "glink25", // github login name, not user name
     repo: "glink25.github.io", //"urodele",
-    logInUrl: "https://github-login.link-ai.workers.dev?redirect_uri=https://glink25.github.io/login",
-    logInAuthUrl: "https://github-login.link-ai.workers.dev/",
+    logInUrl:
+      "https://github-login.link-ai.workers.dev/api/oauth/authorize?redirect_uri=https://glink25.github.io/login",
+    logInAuthUrl: "https://github-login.link-ai.workers.dev/api/oauth/token",
   },
   head: {
     title: "Urodele",
